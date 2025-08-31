fa321f21fc50a0c9d59de93a2050a510b2fb4f2c
Carlo Arellano <carlo.arellano@hotmail.com>
2025-08-31 23:49:21 +0800

    Initial works in chapter 4

---
+++ cs13net9/C04_WritingFunctions/Program.cs
@@ -0,0 +1,19 @@
+﻿
+// TimesTable(7, 20);
+
+//ConfigureConsole(useComputerCulture: true);
+//ConfigureConsole(culture: "fr-FR")
+//decimal taxToPay = CalculateTax(amount: 149, twoLetterRegionCode: "FR");
+//WriteLine($"You must pay {taxToPay:C} in tax.");
+
+// Alternatively, call the function in the interpolated string.
+// WriteLine($"You must pay {CalculateTax(amount: 149,
+// twoLetterRegionCode: "FR"):C} in tax.");
+
+//RunCardinalToOrdinal();
+
+//RunFactorial();
+
+//RunFibImperative();
+
+RunFibFunctional();
