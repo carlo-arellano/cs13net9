1f148e2696b0667fc5371153cfb8516e26a95fc5
Carlo Arellano <carlo.arellano@hotmail.com>
2025-09-13 22:52:02 +0800

    Works on chapter 5

---
+++ cs13net9/C05_PacktLibraryModern/Equality.cs
@@ -0,0 +1,11 @@
+﻿namespace C05_PacktLibraryModern;
+
+public class AnimalClass
+{
+    public string? Name { get; set; }
+}
+
+public record AnimalRecord
+{
+    public string? Name { get; set; }
+}
--- cs13net9/C05_PeopleApp/Program.cs
+++ cs13net9/C05_PeopleApp/Program.cs
@@ -1,6 +1,6 @@
 ﻿using C05_PacktLibraryModern;
 using C05_PacktLibraryNet2;
-
+using Microsoft.VisualBasic;
 using Fruit = (string Name, int Number); // Aliasing a tuple type.
 
 ConfigureConsole(); // Sets current culture to US English.
@@ -162,3 +162,307 @@ WriteLine(format: "{0} of {1} was created at {2:hh:mm:ss} on a {2:dddd}.",
 
 #endregion
 
+#region Returning values from methods
+
+bob.WriteToConsole();
+WriteLine(bob.GetOrigin());
+
+#endregion
+
+#region Defining and passing parameters to methods
+
+WriteLine(bob.SayHello());
+WriteLine(bob.SayHello("Emily"));
+
+#endregion
+
+#region Passing optional parameters
+
+WriteLine(bob.OptionalParameters(3));
+
+WriteLine(bob.OptionalParameters(3, "Jump!", 98.5));
+
+#endregion
+
+#region Naming parameter values when calling methods
+
+WriteLine(bob.OptionalParameters(3, number: 52.7, command: "Hide!"));
+
+WriteLine(bob.OptionalParameters(3, "Poke!", active: false));
+
+#endregion
+
+#region Controlling how parameters are passed
+
+int a = 10;
+int b = 20;
+int c = 30;
+int d = 40;
+
+WriteLine($"Before: a={a}, b={b}, c={c}, d={d}");
+
+bob.PassingParameters(a, b, ref c, out d);
+
+WriteLine($"After: a={a}, b={b}, c={c}, d={d}");
+
+int e = 50;
+int f = 60;
+int g = 70;
+WriteLine($"Before: e={e}, f={f}, g={g}, h doesn't exist yet!");
+
+// Simplified C# 7 or later syntax for the out parameter.
+bob.PassingParameters(e, f, ref g, out int h);
+WriteLine($"After: e={e}, f={f}, g={g}, h={h}");
+
+#endregion
+
+#region Passing a variable number of parameters
+
+bob.ParamsParameters("Sum using commas", 3, 6, 1, 2);
+bob.ParamsParameters("Sum using collection expression", [3, 6, 1, 2]);
+bob.ParamsParameters("Sum using explicit array", new int[] { 3, 6, 1, 2 });
+bob.ParamsParameters("Sum (empty)");
+
+#endregion
+
+#region Combining multiple returned values using tuples
+
+(string, int) fruit = bob.GetFruit();
+WriteLine($"{fruit.Item1}, {fruit.Item2} there are.");
+
+#endregion
+
+#region Naming the fields of a tuple
+
+// Without an aliased tuple type.
+//var fruitNamed = bob.GetNamedFruit();
+
+// With an aliased tuple type.
+Fruit fruitNamed = bob.GetNamedFruit();
+WriteLine($"There are {fruitNamed.Number} {fruitNamed.Name}.");
+
+var thing1 = ("Neville", 4);
+WriteLine($"{thing1.Item1} has {thing1.Item2} children.");
+
+var thing2 = (bob.Name, bob.Children.Count);
+WriteLine($"{thing2.Name} has {thing2.Count} children.");
+
+#endregion
+
+#region Deconstructing tuples
+
+(string fruitName, int fruitNumber) = bob.GetFruit();
+WriteLine($"Deconstructed: {fruitName}, {fruitNumber}");
+
+#endregion
+
+#region Deconstructing other types using tuples
+
+var (name1, dob1) = bob; // Implicitly calls the Deconstruct method.
+WriteLine($"Deconstructed person: {name1}, {dob1}");
+
+var (name2, dob2, fav2) = bob;
+WriteLine($"Deconstructed person: {name2}, {dob2}, {fav2}");
+
+#endregion
+
+#region Implementing functionality using local functions
+
+// Change to -1 to make the exception handling code execute.
+int number = 5;
+
+try
+{
+    WriteLine($"{number}! is {Person.Factorial(number)}");
+}
+catch (Exception ex)
+{
+    WriteLine($"{ex.GetType()} says: {ex.Message} number was {number}.");
+}
+
+#endregion
+
+#region Defining read-only properties
+
+Person sam = new()
+{
+    Name = "Sam",
+    Born = new(1969, 6, 25, 0, 0, 0, TimeSpan.Zero)
+};
+
+WriteLine(sam.Origin);
+WriteLine(sam.Greeting);
+WriteLine(sam.Age);
+
+#endregion
+
+#region Defining settable properties
+
+sam.FavoriteIceCream = "Chocolate Fudge";
+WriteLine($"Sam's favorite ice-cream flavor is {sam.FavoriteIceCream}.");
+
+string color = "Black";
+
+try
+{
+    sam.FavoritePrimaryColor = color;
+    WriteLine($"Sam's favorite primary color is {sam.FavoritePrimaryColor}.");
+}
+catch (Exception ex)
+{
+    WriteLine("Tried to set {0} to '{1}': {2}",
+      nameof(sam.FavoritePrimaryColor), color, ex.Message);
+}
+
+#endregion
+
+#region Defining indexers
+
+sam.Children.Add(new()
+{
+    Name = "Charlie",
+    Born = new(2010, 3, 18, 0, 0, 0, TimeSpan.Zero)
+});
+
+sam.Children.Add(new()
+{
+    Name = "Ella",
+    Born = new(2020, 12, 24, 0, 0, 0, TimeSpan.Zero)
+});
+
+// Get using Children list.
+WriteLine($"Sam's first child is {sam.Children[0].Name}.");
+WriteLine($"Sam's second child is {sam.Children[1].Name}.");
+
+// Get using the int indexer.
+WriteLine($"Sam's first child is {sam[0].Name}.");
+WriteLine($"Sam's second child is {sam[1].Name}.");
+
+// Get using the string indexer.
+WriteLine($"Sam's child named Ella is {sam["Ella"].Age} years old.");
+
+#endregion
+
+#region Pattern matching flight passengers
+
+// An array containing a mix of passenger types.
+Passenger[] passengers = {
+  new FirstClassPassenger { AirMiles = 1_419, Name = "Suman" },
+  new FirstClassPassenger { AirMiles = 16_562, Name = "Lucy" },
+  new BusinessClassPassenger { Name = "Janice" },
+  new CoachClassPassenger { CarryOnKG = 25.7, Name = "Dave" },
+  new CoachClassPassenger { CarryOnKG = 0, Name = "Amit" },
+};
+
+foreach (Passenger passenger in passengers)
+{
+    decimal flightCost = passenger switch
+    {
+        /* C# 8 syntax
+        FirstClassPassenger p when p.AirMiles > 35_000 => 1_500M,
+        FirstClassPassenger p when p.AirMiles > 15_000 => 1_750M,
+        FirstClassPassenger _                          => 2_000M,*/
+
+        // C# 9 or later syntax
+        FirstClassPassenger p => p.AirMiles switch
+        {
+            > 35_000 => 1_500M,
+            > 15_000 => 1_750M,
+            _ => 2_000M
+        },
+        BusinessClassPassenger _ => 1_000M,
+        CoachClassPassenger p when p.CarryOnKG < 10.0 => 500M,
+        CoachClassPassenger _ => 650M,
+        _ => 800M
+    };
+    WriteLine($"Flight costs {flightCost:C} for {passenger}");
+}
+
+#endregion
+
+#region Init-only properties
+
+ImmutablePerson jeff = new()
+{
+    FirstName = "Jeff",
+    LastName = "Winger"
+};
+//jeff.FirstName = "Geoff";
+
+#endregion
+
+#region Defining record types
+
+ImmutableVehicle car = new()
+{
+    Brand = "Mazda MX-5 RF",
+    Color = "Soul Red Crystal Metallic",
+    Wheels = 4
+};
+
+ImmutableVehicle repaintedCar = car with { Color = "Polymetal Grey Metallic" };
+
+WriteLine($"Original car color was {car.Color}.");
+WriteLine($"New car color is {repaintedCar.Color}.");
+
+#endregion
+
+#region Equality of record types
+
+AnimalClass ac1 = new() { Name = "Rex" };
+AnimalClass ac2 = new() { Name = "Rex" };
+
+WriteLine($"ac1 == ac2: {ac1 == ac2}");
+
+AnimalRecord ar1 = new() { Name = "Rex" };
+AnimalRecord ar2 = new() { Name = "Rex" };
+
+WriteLine($"ar1 == ar2: {ar1 == ar2}");
+
+#endregion
+
+#region Equality of other types
+
+int number1 = 3;
+int number2 = 3;
+WriteLine($"number1: {number1}, number2: {number2}");
+WriteLine($"number1 == number2: {number1 == number2}");
+
+Person p1 = new() { Name = "Kevin" };
+Person p2 = new() { Name = "Kevin" };
+WriteLine($"p1: {p1}, p2: {p2}");
+WriteLine($"p1.Name: {p1.Name}, p2.Name: {p2.Name}");
+WriteLine($"p1 == p2: {p1 == p2}");
+
+Person p3 = p1;
+WriteLine($"p3: {p3}");
+WriteLine($"p3.Name: {p3.Name}");
+WriteLine($"p1 == p3: {p1 == p3}");
+
+// string is the only class reference type implemented to 
+// act like a value type for equality.
+WriteLine($"p1.Name: {p1.Name}, p2.Name: {p2.Name}");
+WriteLine($"p1.Name == p2.Name: {p1.Name == p2.Name}");
+
+#endregion
+
+#region Positional data members in records
+
+ImmutableAnimal oscar = new("Oscar", "Labrador");
+var (who, what) = oscar; // Calls the Deconstruct method.
+WriteLine($"{who} is a {what}.");
+
+#endregion
+
+#region Defining a primary constructor
+
+Headset vp = new("Apple", "Vision Pro");
+WriteLine($"{vp.ProductName} is made by {vp.Manufacturer}.");
+
+Headset holo = new();
+WriteLine($"{holo.ProductName} is made by {holo.Manufacturer}.");
+
+Headset mq = new() { Manufacturer = "Meta", ProductName = "Quest 3" };
+WriteLine($"{mq.ProductName} is made by {mq.Manufacturer}.");
+
+#endregion
