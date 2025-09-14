0a8556ee1a30b3efa49607c52b7f33767e70526b
Carlo Arellano <carlo.arellano@hotmail.com>
2025-09-14 12:50:17 +0800

    Works on chapter 6

---
+++ cs13net9/C06_PeopleApp/Program.EventHandler.cs
@@ -0,0 +1,22 @@
+﻿using C06_PacktLibrary;
+
+partial class Program
+{
+    // A method to handle the Shout event received by the harry object.
+    private static void Harry_Shout(object? sender, EventArgs e)
+    {
+        // If no sender, then do nothing.
+        if (sender is null) return;
+
+        // If sender is not a Person, then do nothing.
+        if (sender is not Person p) return;
+
+        WriteLine($"{p.Name} is this angry: {p.AngerLevel}.");
+    }
+
+    // Another method to handle the event received by the harry object.
+    private static void Harry_Shout_2(object? sender, EventArgs e)
+    {
+        WriteLine("Stop it!");
+    }
+}
---
+++ cs13net9/C06_PeopleApp/Program.cs
@@ -0,0 +1,156 @@
+﻿#region Implementing functionality using methods and operators
+
+using C06_PacktLibrary;
+
+Person harry = new()
+{
+    Name = "Harry",
+    Born = new(year: 2001, month: 3, day: 25,
+    hour: 0, minute: 0, second: 0,
+    offset: TimeSpan.Zero)
+};
+
+harry.WriteToConsole();
+
+// Implementing functionality using methods.
+Person lamech = new() { Name = "Lamech" };
+Person adah = new() { Name = "Adah" };
+Person zillah = new() { Name = "Zillah" };
+
+// Call the instance method to marry Lamech and Adah.
+lamech.Marry(adah);
+
+// Call the static method to marry Lamech and Zillah.
+//Person.Marry(lamech, zillah);
+
+if (lamech + zillah)
+{
+    WriteLine($"{lamech.Name} and {zillah.Name} successfully got married.");
+}
+
+lamech.OutputSpouses();
+adah.OutputSpouses();
+zillah.OutputSpouses();
+
+// Call the instance method to make a baby.
+Person baby1 = lamech.ProcreateWith(adah);
+baby1.Name = "Jabal";
+WriteLine($"{baby1.Name} was born on {baby1.Born}");
+
+// Call the static method to make a baby.
+Person baby2 = Person.Procreate(zillah, lamech);
+baby2.Name = "Tubalcain";
+
+// Use the * operator to "multiply".
+Person baby3 = lamech * adah;
+baby3.Name = "Jubal";
+
+Person baby4 = zillah * lamech;
+baby4.Name = "Naamah";
+
+adah.WriteChildrenToConsole();
+zillah.WriteChildrenToConsole();
+lamech.WriteChildrenToConsole();
+
+for (int i = 0; i < lamech.Children.Count; i++)
+{
+    WriteLine(format: "  {0}'s child #{1} is named \"{2}\".",
+      arg0: lamech.Name, arg1: i,
+      arg2: lamech.Children[i].Name);
+}
+
+#endregion
+
+#region Working with non-generic types
+
+// Non-generic lookup collection.
+System.Collections.Hashtable lookupObject = new();
+lookupObject.Add(key: 1, value: "Alpha");
+lookupObject.Add(key: 2, value: "Beta");
+lookupObject.Add(key: 3, value: "Gamma");
+lookupObject.Add(key: harry, value: "Delta");
+
+int key = 2; // Look up the value that has 2 as its key.
+
+WriteLine(format: "Key {0} has value: {1}",
+  arg0: key,
+  arg1: lookupObject[key]);
+
+// Look up the value that has harry as its key.
+WriteLine(format: "Key {0} has value: {1}",
+  arg0: harry,
+  arg1: lookupObject[harry]);
+
+#endregion
+
+#region Working with generic types
+
+// Define a generic lookup collection.
+Dictionary<int, string> lookupIntString = new();
+lookupIntString.Add(key: 1, value: "Alpha");
+lookupIntString.Add(key: 2, value: "Beta");
+lookupIntString.Add(key: 3, value: "Gamma");
+lookupIntString.Add(key: 4, value: "Delta");
+
+key = 3;
+
+WriteLine(format: "Key {0} has value: {1}",
+  arg0: key,
+  arg1: lookupIntString[key]);
+
+#endregion
+
+#region Defining and handling delegates
+
+// Assign the method(s) to the Shout event delegate.
+harry.Shout += Harry_Shout;
+harry.Shout += Harry_Shout_2;
+
+// Call the Poke method that eventually raises the Shout event.
+harry.Poke();
+harry.Poke();
+harry.Poke();
+harry.Poke();
+
+#endregion
+
+#region Comparing objects when sorting
+
+Person?[] people =
+{
+  null,
+  new() { Name = "Simon" },
+  new() { Name = "Jenny" },
+  new() { Name = "Adam" },
+  new() { Name = null },
+  new() { Name = "Richard" }
+};
+
+OutputPeopleNames(people, "Initial list of people:");
+
+Array.Sort(people);
+
+OutputPeopleNames(people, "After sorting using Person's IComparable implementation:");
+
+Array.Sort(people, new PersonComparer());
+
+OutputPeopleNames(people, "After sorting using PersonComparer's IComparer implementation:");
+
+#endregion
+
+//#region Defining struct types
+
+//DisplacementVector dv1 = new(3, 5);
+//DisplacementVector dv2 = new(-2, 7);
+//DisplacementVector dv3 = dv1 + dv2;
+
+//WriteLine($"({dv1.X}, {dv1.Y}) + ({dv2.X}, {dv2.Y}) = ({dv3.X}, {dv3.Y})");
+
+//DisplacementVector dv4 = new();
+//WriteLine($"({dv4.X}, {dv4.Y})");
+
+//DisplacementVector dv5 = new(3, 5);
+//WriteLine($"dv1.Equals(dv5): {dv1.Equals(dv5)}");
+//WriteLine($"dv1 == dv5: {dv1 == dv5}");
+
+//#endregion
